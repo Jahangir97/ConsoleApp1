fb8e9633827b24cd99ada832cc74a1d7a8400a47
Jahanda97 <jahangirjn@code.edu.az>
2022-04-13 16:50:56 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Console.WriteLine("ededi daxil edin ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ikinci ededi daxil edin ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            if (sayi > sayi2) 
+            {
+                Console.WriteLine("birinci ikinciden boyukdur");
+            } 
+            else 
+            {
+                Console.WriteLine("ikinci boyukdur");
+            }
+
+            
+            
+            
+            
+            
+            /* int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("sayi cutdur");
+            }
+            else
+            {
+                Console.WriteLine("sayi tekdir");
+            }
+
+
+
+
+        }
+    }
+}
